287d2041420f73bbe424c4e0d3af4c475115de76
Eduardo <eduguimara@gmail.com>
2023-12-29 22:00:46 -0300

    Initial commit

---
+++ src/main/java/com/devedu/videolistproject/VideoListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devedu.videolistproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoListProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devedu/videolistproject/VideoListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devedu.videolistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
