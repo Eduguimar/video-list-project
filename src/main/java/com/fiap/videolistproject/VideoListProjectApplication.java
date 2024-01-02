add61d37b6808c14fdc94465bf8ec49bda97a908
Eduardo <eduguimara@gmail.com>
2024-01-02 20:25:33 -0300

    Renaming package

--- src/main/java/com/fiap/videolistproject/VideoListProjectApplication.java
+++ src/main/java/com/fiap/videolistproject/VideoListProjectApplication.java
@@ -1,4 +1,4 @@
-package com.devedu.videolistproject;
+package com.fiap.videolistproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
